da9cf4c4d39eaaf64f1aff57a65fef147fb39cb3
ridwanrahman <ridwan_rahman@ymail.com>
2021-01-29 00:50:40 +1100

    refactor

--- laravel-api/tests/TestCase.php
+++ laravel-api/tests/TestCase.php
@@ -3,8 +3,10 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tymon\JWTAuth\Foundation\Testing\WithFaker;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
 }
